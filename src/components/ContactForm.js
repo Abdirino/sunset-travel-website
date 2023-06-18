9680628157a7a1c235e2efec9f5e1aa85d236a37
Abdirino <abdirinoahash@gmail.com>
2023-06-18 09:24:19 +0300

    contact sec revamp

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,21 @@
 import "./ContactFormStyle.css";
 
 function ContactForm() {
-    return(
-        <div className="form-container">
-            <h1>Message Us!</h1>
-            <form>
-                <input placeholder="Name*"/>
-                <input placeholder="Email*"/>
-                <input placeholder="Subject"/>
-                <textarea placeholder="Message*" rows="4"></textarea>
-                <button>Send</button>
-            </form>
-        </div>
-    )
+  return (
+    <>
+      <div className="form-container">
+        <div className="formDetails"></div>
+        <form>
+          <h1>Message Us!</h1>
+          <input placeholder="Name*" />
+          <input placeholder="Email*" />
+          <input placeholder="Subject" />
+          <textarea placeholder="Message*" rows="4"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default ContactForm;
