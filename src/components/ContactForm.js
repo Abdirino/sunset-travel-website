43305cd74e55ecbdfef39c0ac382b7746a2c8134
Abdirino <abdirinoahash@gmail.com>
2023-06-19 15:30:55 +0300

    contact sec done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,31 @@
 import "./ContactFormStyle.css";
+import ContactImg from "../images/ContactIMG.jpg";
 
 function ContactForm() {
   return (
     <>
       <div className="form-container">
-        <div className="formDetails"></div>
+        <div className="formDetails">
+          <div className="c-img">
+            <img src={ContactImg} alt="" />
+          </div>
+          {/* <div className="c-details">
+            <div className="detail">
+              <h2>Paris Office</h2>
+              <p>198 West 21th Street</p>
+              <p>New York NY 10010</p>
+              <p>Phone: +88 (0) 101 0000 000</p>
+              <p>Email: goldenSunset@qode.com</p>
+            </div>
+            <div className="detail">
+              <h2>Istanbul Office</h2>
+              <p>198 West 21th Street</p>
+              <p>New York NY 10010</p>
+              <p>Phone: +88 (0) 101 0000 000</p>
+              <p>Email: goldenSunset@qode.com</p>
+            </div>
+          </div> */}
+        </div>
         <form>
           <h1>Message Us!</h1>
           <input placeholder="Name*" />
